ffc03f3df84e9d9d96040142a164e6f6291ca8f1
ACBradley7 <129814955+ACBradley7@users.noreply.github.com>
2023-04-03 21:33:23 -0500

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,61 @@
+function displayVideo() {
+    deleteButton();
+    enlargePage();
+    positionFooter();
+    enlargeDiv();
+    embedVideo();
+    updateCounter();
+}
+
+function deleteButton() {
+    var buttonElement = document.getElementById("theButton");
+    buttonElement.remove();
+}
+
+function enlargePage() {
+    var page = document.getElementById("pageDiv");
+    page.style.height = "1100px";
+}
+
+function positionFooter() {
+    var footer = document.getElementById("footerDiv");
+    footer.style.backgroundColor = "aqua";
+}
+
+function enlargeDiv() {
+    var bodyDiv = document.getElementById("bodyDiv");
+    bodyDiv.style.height = "700px";
+}
+
+function embedVideo() {
+    var videoFrame = document.getElementById("bodyDiv");
+    var iFrame = document.createElement("iframe");
+    iFrame.style.width = "100%";
+    iFrame.style.height = "100%";
+    iFrame.setAttribute("frameborder", "0");
+    iFrame.setAttribute("src", "https://www.youtube.com/embed/dQw4w9WgXcQ");
+    videoFrame.appendChild(iFrame);
+}
+
+function updateCounter() {
+    var persistentCounter = 0;
+    var counterDiv = document.getElementById("counterDiv")
+    var counterString = document.getElementById("counterText");
+    var counterText = counterString.textContent;
+    var counterInt = parseInt(counterText);
+    counterInt = counterInt + 1;
+    counterString.innerHTML = counterInt;
+    localStorage.setItem("persistentCounter", counterInt);
+    console.log(counterInt);
+}
+
+function getCounter() {
+    var counter = localStorage.getItem("persistentCounter");
+    console.log(counter);
+    counter = parseInt(counter);
+    if (counter == "NaN") {
+        counter = 0;
+    }
+    var counterString = document.getElementById("counterText");
+    counterString.innerHTML = counter;
+}
